d70999ffa0c1b994fcc52865647ea9b228be9ad6
PepeteLaMouette <lilianbouvier.music@gmail.com>
2020-06-23 09:48:20 +0200

    Updates

---
+++ precache-manifest.eca1df83c8a5a2829622c6becd55ce84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2446be27d553c8aef1b11e1910097ec2",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "b05f666e2a7239496635",
+    "url": "/react-hangman/static/css/main.e0440e32.chunk.css"
+  },
+  {
+    "revision": "ff0615ef65db9a6e973d",
+    "url": "/react-hangman/static/js/2.b494c0fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hangman/static/js/2.b494c0fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05f666e2a7239496635",
+    "url": "/react-hangman/static/js/main.83e67ffe.chunk.js"
+  },
+  {
+    "revision": "04862e76dd907e8f4b6c",
+    "url": "/react-hangman/static/js/runtime-main.e1105988.js"
+  }
+]);
